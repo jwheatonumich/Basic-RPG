7bec1fffa2e8cbc9b7839890218cae883f8acaa6
James Heaton <jwheatonumich@gmail.com>
2020-12-17 11:04:31 -0600

    Added double squirrel
    Also updated order of trainer options

--- battle/battle-select.js
+++ battle/battle-select.js
@@ -1,10 +1,10 @@
 //Array of enemy names
-var enemies = ["A Squirrel","Wimpy Wombat","Ordinary Otter","Precarious Porcupine","Riku the Bear"];
+var enemies = ["A Squirrel","Two Squirrels","Ordinary Otter","Precarious Porcupine","Riku the Bear"];
 
 //Array of enemy images
 var pictureList = [
     "../images/squirrel-avatar-mini.png",
-    "../images/wimpy-wombat.png",
+    "../images/two-squirrels-mini.png",
     "../images/ordinary-otter.png",
     "../images/precarious-porcupine.png",
     "../images/bear-avatar.png" ];
--- battle/main-battle.js
+++ battle/main-battle.js
@@ -41,8 +41,8 @@ function dataLoad(){
 
     stats = [
         {"name":"A Squirrel", "enemyID":"0001", "health":50, "maxhealth":50, "attack":10, "defense":5, "acorncoin":1, "mushroomcoin":0, "bearclawcoin":0 },
+        {"name":"Two Squirrels", "enemyID":"0003", "health":100, "maxhealth":100, "attack":20, "defense":5, "acorncoin":3,"mushroomcoin":0, "bearclawcoin":0 } ,
         {"name":"Ordinary Otter", "enemyID":"0002", "health":100, "maxhealth":100, "attack":15, "defense":7, "acorncoin":2, "mushroomcoin":0, "bearclawcoin":0 } ,
-        {"name":"Wimpy Wombat", "enemyID":"0003", "health":200, "maxhealth":200, "attack":20, "defense":10, "acorncoin":2,"mushroomcoin":1, "bearclawcoin":0 } ,
         {"name":"Precarious Porcupine", "enemyID":"0003", "health":50, "maxhealth":50, "attack":100, "defense":10, "acorncoin":10, "mushroomcoin":2, "bearclawcoin":0 } ,
         {"name":"Riku the Bear", "enemyID":"0004", "health":400, "maxhealth":400, "attack":50, "defense":50, "acorncoin":20, "mushroomcoin":5, "bearclawcoin":1 }
     ]
