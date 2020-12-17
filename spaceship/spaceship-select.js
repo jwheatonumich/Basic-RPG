623216a243fbe8ed032acd19a1e74a9dd907144a
James Heaton <jwheatonumich@gmail.com>
2020-12-17 08:27:02 -0600

    Added spaceship crash site
    Also added the "day" stat for the player.

---
+++ spaceship/spaceship-select.js
@@ -0,0 +1,37 @@
+//Load player stats in localstorage
+var retrievedObject = localStorage.getItem('storedPlayerStats');
+var playerStats = JSON.parse(retrievedObject)
+
+//Load each player stat into a variable
+function playerSetup() {
+    playerName = playerStats.name;
+    playerHealth = playerStats.health;
+    playerMaxHealth = playerStats.maxhealth;
+    playerAttack = playerStats.attack;
+    playerDefense = playerStats.defense;
+    playerAcornCoin = playerStats.acorncoin;
+    playerMushroomCoin = playerStats.mushroomcoin;
+    playerBearclawCoin = playerStats.bearclawcoin;
+    leafCoin = playerStats.leafcoin;
+}
+
+//Function that sets text on the website equal to various stat variables
+function setStats() {
+    
+    document.getElementById("acorn-coin").innerHTML = playerAcornCoin;
+    document.getElementById("mushroom-coin").innerHTML = playerMushroomCoin;
+    document.getElementById("bearclaw-coin").innerHTML = playerBearclawCoin;
+    document.getElementById("leaf-coin").innerHTML = leafCoin;
+}
+
+//Add random numbers of coins to the player's inventory
+function sleep() {
+    
+    playerStats["day"] +=1;
+    document.getElementById("textbox").innerHTML = 'Day '+ playerStats["day"] + '<br><br>You wake up. You don\'t feel very refreshed.<br><br> Where is the coffee?'
+    localStorage.setItem('storedPlayerStats', JSON.stringify(playerStats));
+}
+
+//Populate player stats on page load
+window.onload = playerSetup();
+window.onload = setStats();
