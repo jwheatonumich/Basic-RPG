c7c678d53b8c81ef098565a063f0864aab58b941
James Heaton <jwheatonumich@gmail.com>
2020-12-18 06:56:40 -0600

    Updated Player Stats
    Added endurance stat, maxhealth = 4x endurance.
    Updated starting stats to be lower.

--- settings/settings-script.js
+++ settings/settings-script.js
@@ -4,7 +4,7 @@ var playerStats = JSON.parse(retrievedObject)
 
 //Function to set player stats variable to new game stats
 function dataLoad(){
-    playerStats = {"name":"Fred", "health":100, "maxhealth":100, "attack":10, "defense":5,  
+    playerStats = {"name":"Fred", "health":40, "maxhealth":40, "attack":10, "defense":10, "endurance":10, 
     "day":1, "caveday":0,"treeday":0,
     "acorncoin":0, "mushroomcoin":0, "bearclawcoin":0, "leafcoin":10}
     playerStats["name"] = document.getElementById("name").value
--- train/train-select.js
+++ train/train-select.js
@@ -2,75 +2,53 @@
 var retrievedObject = localStorage.getItem('storedPlayerStats');
 var playerStats = JSON.parse(retrievedObject)
 
-/*
-//Array of stats
-var trainingOptions = ["Health","Attack","Defense"];
-var trainingOutputs = ["maxhealth","attack","defense"]
-
-//Store the dropdown element
-selectMenu = document.getElementById("select-training")
-
-//Create drop-down from enemy name array
-function popDropdown() {
-    for(element in trainingOptions)
-        {
-        var opt = document.createElement("option");
-        opt.value= trainingOutputs[element];
-        opt.innerHTML = trainingOptions[element];
-
-        //Append to the dropdown
-        selectMenu.appendChild(opt);
-        }
-}
-
-function startTraining(){
-    var trainingSelect = document.getElementById("select-training");
-    var chosenTraining = trainingSelect.value;
-    if (playerStats["acorncoin"] >= 5){
-        playerStats[chosenTraining] +=1;
-        playerStats["acorncoin"] -=5;
-        localStorage.setItem('storedPlayerStats', JSON.stringify(playerStats));
-        playerSetup();
-        setStats();
-    }
-}
-*/
-
+//function to set player stats based on the button clicked
 function acornTraining(stat){
     if (playerStats["acorncoin"] >= 3){
         playerStats[stat] +=1;
         playerStats["acorncoin"] -=3;
+        healthCalc();
         localStorage.setItem('storedPlayerStats', JSON.stringify(playerStats));
         playerSetup();
         setStats();
     } 
 }
 
+//function to set player stats based on the button clicked
 function mushroomTraining(stat){
     if (playerStats["mushroomcoin"] >= 2){
         playerStats[stat] +=1;
         playerStats["mushroomcoin"] -=2;
+        healthCalc();
         localStorage.setItem('storedPlayerStats', JSON.stringify(playerStats));
         playerSetup();
         setStats();
     } 
 }
 
+//function to set player stats based on the button clicked
 function bearclawTraining(stat){
     if (playerStats["bearclawcoin"] >= 1){
         playerStats[stat] +=1;
         playerStats["bearclawcoin"] -=1;
+        healthCalc();
         localStorage.setItem('storedPlayerStats', JSON.stringify(playerStats));
         playerSetup();
         setStats();
     } 
 }
+
+//Function to calculate max health from endurance
+function healthCalc(){
+    playerStats["maxhealth"] = 4 * playerStats["endurance"]
+};
 
 //Load each player stat into a variable
 function playerSetup() {
     playerName = playerStats.name;
     playerHealth = playerStats.health;
     playerMaxHealth = playerStats.maxhealth;
+    playerEndurance = playerStats.endurance;
     playerAttack = playerStats.attack;
     playerDefense = playerStats.defense;
     playerAcornCoin = playerStats.acorncoin;
@@ -82,19 +60,16 @@ function playerSetup() {
 //Function that sets text on the website equal to various stat variables
 function setStats() {
     
-    document.getElementById("player-name").innerHTML = playerName;
     document.getElementById("character-stats").innerHTML = 
-        'Health: ' + playerHealth + '/' +  playerMaxHealth + '<br />' +
         'Attack: ' + playerAttack + '<br />' +
-        'Defense: ' + playerDefense
+        'Defense: ' + playerDefense + '<br />' +
+        'Endurance: ' + playerEndurance + '<br />' +
+        'Health: ' + playerMaxHealth
     document.getElementById("acorn-coin").innerHTML = playerAcornCoin;
     document.getElementById("mushroom-coin").innerHTML = playerMushroomCoin;
     document.getElementById("bearclaw-coin").innerHTML = playerBearclawCoin;
     document.getElementById("leaf-coin").innerHTML = leafCoin;
 }
-
-//Populate dropdown on page load
-//window.onload = popDropdown();
 
 //Populate player stats on page load
 window.onload = playerSetup();
