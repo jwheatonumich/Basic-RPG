e18f64d6672de9ec4cf53941da44f1d22204d294
James Heaton <jwheatonumich@gmail.com>
2020-12-20 18:40:07 -0600

    Prevented caching
    Added meta tags to prevent caching
    
    Fixed the bear eyes script (again..?)

--- cave/cave-select.js
+++ cave/cave-select.js
@@ -66,7 +66,7 @@ function enterCave() {
         document.getElementById("textbox").appendChild(elem);
 
         //Change the image after user tries to steal a coin
-        document.getElementById("cave-image").src = "../images/bear-cave-eyes-imagemap.png"
+        document.getElementById("page-image").src = "../images/bear-cave-eyes-imagemap.png"
 
     }else{
         //Start the battle
