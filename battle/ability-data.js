db2a8c21b6ad31ffaaaf87368a14c3e8eaad965a
James Heaton <jwheatonumich@gmail.com>
2020-12-25 20:47:37 -0600

    Added ability data
    Added ability data in an array

---
+++ battle/ability-data.js
@@ -0,0 +1,82 @@
+abilityData =
+{
+    "attack":
+    {
+        //Current turn damage modifiers
+        "selfAttackMultiplier":1, "opponentAttackMultiplier":1, "selfDefenseMultiplier":1, "opponentDefenseMultiplier":1,
+        //Multi-turn damage modifiers
+        "selfAttack":1, "opponentAttack":1, "selfDefense":1, "opponentDefense":1,
+        //Other efects
+        "armor":0, "stun":0, "poison":0, "priority":false
+    },
+
+    "charge":
+    {
+        //Current turn damage modifiers
+        "selfAttackMultiplier":1.2, "opponentAttackMultiplier":1, "selfDefenseMultiplier":0.8, "opponentDefenseMultiplier":1,
+        //Multi-turn damage modifiers
+        "selfAttack":1, "opponentAttack":1, "selfDefense":1, "opponentDefense":1,
+        //Other efects
+        "armor":0, "stun":0, "poison":0, "priority":false
+    },
+
+    "block":
+    {
+        //Current turn damage modifiers
+        "selfAttackMultiplier":0, "opponentAttackMultiplier":1, "selfDefenseMultiplier":2.0, "opponentDefenseMultiplier":1,
+        //Multi-turn damage modifiers
+        "selfAttack":1, "opponentAttack":1, "selfDefense":1, "opponentDefense":1,
+        //Other efects
+        "armor":0, "stun":0, "poison":0, "priority":false
+    },
+
+    "quickAttack":
+    {
+        //Current turn damage modifiers
+        "selfAttackMultiplier":1, "opponentAttackMultiplier":1, "selfDefenseMultiplier":1, "opponentDefenseMultiplier":1,
+        //Multi-turn damage modifiers
+        "selfAttack":1, "opponentAttack":1, "selfDefense":1, "opponentDefense":1,
+        //Other efects
+        "armor":0, "stun":0, "poison":0, "priority":true
+    },
+
+    "powerUp":
+    {
+        //Current turn damage modifiers
+        "selfAttackMultiplier":0, "opponentAttackMultiplier":1, "selfDefenseMultiplier":1, "opponentDefenseMultiplier":1,
+        //Multi-turn damage modifiers
+        "selfAttack":1.2, "opponentAttack":1, "selfDefense":1, "opponentDefense":1,
+        //Other efects
+        "armor":0, "stun":0, "poison":0, "priority":true
+    },
+
+    "shield":
+    {
+        //Current turn damage modifiers
+        "selfAttackMultiplier":0, "opponentAttackMultiplier":1, "selfDefenseMultiplier":1, "opponentDefenseMultiplier":1,
+        //Multi-turn damage modifiers
+        "selfAttack":1, "opponentAttack":1, "selfDefense":1, "opponentDefense":1,
+        //Other efects
+        "armor":5, "stun":0, "poison":0, "priority":false
+    },
+
+    "stun":
+    {
+        //Current turn damage modifiers
+        "selfAttackMultiplier":.5, "opponentAttackMultiplier":1, "selfDefenseMultiplier":1, "opponentDefenseMultiplier":1,
+        //Multi-turn damage modifiers
+        "selfAttack":1, "opponentAttack":1, "selfDefense":1, "opponentDefense":1,
+        //Other efects
+        "armor":0, "stun":.5, "poison":0, "priority":false
+    },
+
+    "poison":
+    {
+        //Current turn damage modifiers
+        "selfAttackMultiplier":0, "opponentAttackMultiplier":1, "selfDefenseMultiplier":1, "opponentDefenseMultiplier":1,
+        //Multi-turn damage modifiers
+        "selfAttack":1, "opponentAttack":1, "selfDefense":1, "opponentDefense":1,
+        //Other efects
+        "armor":0, "stun":0, "poison":3, "priority":false
+    }
+}
