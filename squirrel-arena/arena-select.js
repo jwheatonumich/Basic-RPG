42e88ed9e1b3feb8a63cd44173e40f43a8156d63
James Heaton <jwheatonumich@gmail.com>
2021-01-27 19:45:26 -0600

    Removed hard squirrel arena

--- squirrel-arena/arena-select.js
+++ squirrel-arena/arena-select.js
@@ -1,6 +1,5 @@
 //List of possible enemy IDs
 var enemyListEasy = [0,23,1,24,2,3,5,25];//Repeated numbers control probability of selecting
-var enemyListHard = [3,3,3,4,4,5,25];
 
 //Type of reward given for winstreaks in this arena
 var winstreakReward = "acorncoin";
