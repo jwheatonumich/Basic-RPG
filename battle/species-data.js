f2cd98540190b677083141305b2c0005ceb23e6a
James Heaton <jwheatonumich@gmail.com>
2021-02-20 19:26:46 -0600

    Select character now works
    Selecting a character now adds a border to that species and actually changes the player's species

--- battle/species-data.js
+++ battle/species-data.js
@@ -6,7 +6,7 @@ speciesData
             "attack1Name":"attack","attack2Name":"redLaser","attack3Name":"heal","attack4Name":"none",
             "attack1DisplayName":"Attack","attack2DisplayName":"Red Laser","attack3DisplayName":"Heal","attack4DisplayName":"",
             "attack1":"func.attack('attack')","attack2":"func.attack('redLaser')","attack3":"func.attack('heal')","attack4":"func.empty()",
-            "attackbutton1":"../images/attack-button.png","attackbutton2":"../images/shield-button.png","attackbutton3":"../images/stun-button.png","attackbutton4":"../images/powerup-button.png"
+            "imageLeft":"../images/little-goblin.png"
         },
 
         "squirrel":
@@ -14,7 +14,7 @@ speciesData
             "attack1Name":"attack","attack2Name":"quickAttack","attack3Name":"acornToss","attack4Name":"none",
             "attack1DisplayName":"Attack","attack2DisplayName":"Quick Attack","attack3DisplayName":"Acorn Toss","attack4DisplayName":"",
             "attack1":"func.attack('attack')","attack2":"func.attack('quickAttack')","attack3":"func.attack('acornToss')","attack4":"func.empty()",
-            "attackbutton1":"../images/attack-button.png","attackbutton2":"../images/charge-button.png","attackbutton3":"../images/berserk-button.png","attackbutton4":"../images/quickattack-button.png"
+            "imageLeft":"../images/squirrel.png"
         },
 
         "spider":
@@ -22,7 +22,7 @@ speciesData
             "attack1Name":"poisonJab","attack2Name":"disarmingStrike","attack3Name":"webStrike","attack4Name":"honingStrike",
             "attack1DisplayName":"Poison Jab","attack2DisplayName":"Disarming Strike","attack3DisplayName":"Web Strike","attack4DisplayName":"Honing Strike",
             "attack1":"func.attack('poisonJab')","attack2":"func.attack('disarmingStrike')","attack3":"func.attack('webStrike')","attack4":"func.attack('honingStrike')",
-            "attackbutton1":"../images/attack-button.png","attackbutton2":"../images/charge-button.png","attackbutton3":"../images/berserk-button.png","attackbutton4":"../images/quickattack-button.png"
+            "imageLeft":"../images/black-spider.png"
         },
 
         "mushroom":
@@ -30,7 +30,7 @@ speciesData
             "attack1Name":"attack","attack2Name":"poison","attack3Name":"block","attack4Name":"absorb",
             "attack1DisplayName":"Attack","attack2DisplayName":"Poison","attack3DisplayName":"Block","attack4DisplayName":"Absorb",
             "attack1":"func.attack('attack')","attack2":"func.attack('poison')","attack3":"func.attack('block')","attack4":"func.attack('absorb')",
-            "attackbutton1":"../images/attack-button.png","attackbutton2":"../images/poison-button.png","attackbutton3":"../images/block-button.png","attackbutton4":"../images/absorb-button.png"
+            "imageLeft":"../images/little-mushroom.png"
         },
 
         "bear":
@@ -38,7 +38,7 @@ speciesData
             "attack1Name":"attack","attack2Name":"stun","attack3Name":"charge","attack4Name":"berserk",
             "attack1DisplayName":"Attack","attack2DisplayName":"Stun","attack3DisplayName":"Charge","attack4DisplayName":"Berserk",
             "attack1":"func.attack('attack')","attack2":"func.attack('stun')","attack3":"func.attack('charge')","attack4":"func.attack('berserk')",
-            "attackbutton1":"../images/attack-button.png","attackbutton2":"../images/stun-button.png","attackbutton3":"../images/charge-button.png","attackbutton4":"../images/berserk-button.png"
+            "imageLeft":"../images/small-bear.png"
         },
 
         "sheepdog":
@@ -46,6 +46,6 @@ speciesData
             "attack1Name":"rollout","attack2Name":"powerUp","attack3Name":"berserk","attack4Name":"quickAttack",
             "attack1DisplayName":"Roll Out","attack2DisplayName":"Power-up","attack3DisplayName":"Berserk","attack4DisplayName":"Quick Attack",
             "attack1":"func.attack('rollout')","attack2":"func.attack('powerUp')","attack3":"func.attack('berserk')","attack4":"func.attack('quickAttack')",
-            "attackbutton1":"../images/attack-button.png","attackbutton2":"../images/stun-button.png","attackbutton3":"../images/charge-button.png","attackbutton4":"../images/berserk-button.png"
+            "imageLeft":"../images/sheepdog.png"
         },
     }
